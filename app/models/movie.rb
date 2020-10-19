1736cd5fc1ad7097f2940e27aea6650d6145e361
Christine Luo <christineluo@berkeley.edu>
2020-10-19 23:38:38 +0000

    Part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    return ['G', 'PG', 'PG-13', 'R']
+  end
+  
+  def self.with_ratings(ratings_list)
+    if ratings_list.empty?
+      return Movie.all
+    else
+      return Movie.where('upper(rating) in (?)', ratings_list)
+    end
+  end
 end
